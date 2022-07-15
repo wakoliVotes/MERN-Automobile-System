91f34ee0454c7aec3600fc241668698207f8549c
Scelland <votesdw@yahoo.com>
2022-07-15 12:16:01 +0300

    mern

--- server.js
+++ server.js
@@ -37,5 +37,13 @@ require('./routes/homeOlderCars.js')(app);
       console.log("Successfully connected to the MERNAuto Database");
   })
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'index.html'))
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server is running on Port ${PORT}`));
