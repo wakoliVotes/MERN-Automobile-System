2d6d5b249b66a65136ce17c40a883df559070dcd
Scelland <votesdw@yahoo.com>
2022-07-18 10:04:20 +0300

    mern

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ require('./routes/homeOlderCars.js')(app);
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'index.html'))
   });
 }
